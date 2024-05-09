6a15c38021ad780c3325185b897c674bc2db6a80
root <mathewCodex>
2024-05-09 00:19:19 -0700

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -19,7 +19,7 @@ def island_perimeter(grid):
                 i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
                 j == m - 1 or (m > j + 1 and row[j + 1] == 0),
                 i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
-                j == or row[j - 1] == 0,
+                j == 0 or row[j - 1] == 0,
             )
             perimeter += sum(edges)
     return perimeter
